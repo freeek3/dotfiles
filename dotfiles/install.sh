9568061ec33050f97dbb05e772ead83f0eab6cdb
Lars Mueller <me@larsmueller.me>
2019-01-28 21:06:32 +0100

    installscript added

---
+++ dotfiles/install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "Start Installing"
+
+echo "Updating"
+sudo apt-get update && sudo apt-get upgrade -y
+
+#Check for Updates/Installed Version
+if [ -d "$HOME/dotfiles" ] 
+then 
+    bash "$HOME/dotfiles/update.sh"
+    exit 1
+else
+    git clone https://github.com/freeek3/dotfiles-wsl $HOME/dotfiles
+fi
+
+echo "Installing Zsh and oh-my-zsh"
+
+sudo apt-get install zsh -y
+
+curl -L https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | bash
