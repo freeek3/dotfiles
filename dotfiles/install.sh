6f212b2e372b352c7f866cec24aea3197795d51f
Lars Mueller <me@larsmueller.me>
2019-01-28 21:11:00 +0100

    installscript edited

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -4,15 +4,6 @@ echo "Start Installing"
 echo "Updating"
 sudo apt-get update && sudo apt-get upgrade -y
 
-#Check for Updates/Installed Version
-if [ -e "$HOME/dotfiles/update.sh" ] 
-then 
-    bash "$HOME/dotfiles/update.sh"
-    exit 1
-else
-    git clone https://github.com/freeek3/dotfiles $HOME/dotfiles
-fi
-
 echo "Installing Zsh and oh-my-zsh"
 
 sudo apt-get install zsh -y
